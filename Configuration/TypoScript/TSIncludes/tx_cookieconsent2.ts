529c44c988b4a374767b2692305dde072bdbe80a
Matthias Reinhardt <mre@reinhardt-online.de>
2017-11-22 15:40:44 +0100

    CookieConsent 2

--- Configuration/TypoScript/TSIncludes/tx_cookieconsent2.ts
+++ Configuration/TypoScript/TSIncludes/tx_cookieconsent2.ts
@@ -1,3 +1,43 @@
-plugin.tx_cookieconsent2._LOCAL_LANG.de {
+plugin.tx_testsite {
+
+    settings.cookieconsent {
+        link =
+        theme =
+        domain =
+    }
+    _LOCAL_LANG.de
+        {
     message = Cookies erleichtern die Bereitstellung unserer Dienste. Mit der Nutzung unserer Dienste erklären Sie sich damit einverstanden, dass wir Cookies verwenden.
+    }
+}
+
+page{
+    jsInline {
+        cookieconsent = FLUIDTEMPLATE
+        cookieconsent {
+            file = EXT:testsite/Resources/Private/Templates/CookieConsent/Configuration.html
+
+            templateRootPath = {$plugin.tx_testsite.view.templateRootPath}
+            partialRootPath = {$plugin.tx_testsite.view.partialRootPath}
+            layoutRootPath = {$plugin.tx_testsite.view.layoutRootPath}
+
+            settings {
+                link = {$plugin.tx_testsite.settings.link}
+                container = {$plugin.tx_testsite.settings.container}
+                theme = {$plugin.tx_testsite.settings.theme}
+                path = {$plugin.tx_testsite.settings.path}
+                domain = {$plugin.tx_testsite.settings.domain}
+                expiryDays = {$plugin.tx_testsite.settings.expiryDays}
+            }
+        }
+    }
+    includeCSS {
+        cookieconsent2              = EXT:testsite/Resources/Public/CookieConsent/cookieconsent.min.css
+        cookieconsent2.titel        = CookieConsent 2
+    }
+    includeJSFooter {
+        cookieconsentBase = EXT:testsite/Resources/Public/CookieConsent/cookieconsent.min.js
+        cookieconsentBase.title = CookieConsent 2 Basis
+
+    }
 }
