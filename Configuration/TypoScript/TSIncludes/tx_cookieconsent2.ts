41209521fa048b853f4938cbae5e77b5e7943f56
Matthias Reinhardt <mre@reinhardt-online.de>
2017-11-23 11:00:09 +0100

    CookieConsent 2

--- Configuration/TypoScript/TSIncludes/tx_cookieconsent2.ts
+++ Configuration/TypoScript/TSIncludes/tx_cookieconsent2.ts
@@ -1,40 +1,29 @@
-plugin.tx_testsite {
-
-    settings.cookieconsent {
-        link =
-        theme =
-        domain =
-    }
-    _LOCAL_LANG.de
-        {
-    message = Cookies erleichtern die Bereitstellung unserer Dienste. Mit der Nutzung unserer Dienste erklären Sie sich damit einverstanden, dass wir Cookies verwenden.
-    }
-}
-
-page{
+page {
     jsInline {
-        cookieconsent = FLUIDTEMPLATE
-        cookieconsent {
+        201105 = FLUIDTEMPLATE
+        201105 {
             file = EXT:testsite/Resources/Private/Templates/CookieConsent/Configuration.html
 
             templateRootPath = {$plugin.tx_testsite.view.templateRootPath}
             partialRootPath = {$plugin.tx_testsite.view.partialRootPath}
             layoutRootPath = {$plugin.tx_testsite.view.layoutRootPath}
 
-            settings {
-                link = {$plugin.tx_testsite.settings.link}
-                container = {$plugin.tx_testsite.settings.container}
-                theme = {$plugin.tx_testsite.settings.theme}
-                path = {$plugin.tx_testsite.settings.path}
-                domain = {$plugin.tx_testsite.settings.domain}
-                expiryDays = {$plugin.tx_testsite.settings.expiryDays}
+            settings.cookieconsent {
+                link = {$plugin.tx_testsite.settings.cookieconsent.link}
+                container = {$plugin.tx_testsite.settings.cookieconsent.container}
+                theme = {$plugin.tx_testsite.settings.cookieconsent.theme}
+                path = {$plugin.tx_testsite.settings.cookieconsent.path}
+                domain = {$plugin.tx_testsite.settings.cookieconsent.domain}
+                expiryDays = {$plugin.tx_testsite.settings.cookieconsent.expiryDays}
             }
         }
     }
+
     includeCSS {
-        cookieconsent2              = EXT:testsite/Resources/Public/CookieConsent/cookieconsent.min.css
-        cookieconsent2.titel        = CookieConsent 2
+        cookieconsent2 = EXT:testsite/Resources/Public/CookieConsent/cookieconsent.min.css
+        cookieconsent2.titel = CookieConsent 2
     }
+
     includeJSFooter {
         cookieconsentBase = EXT:testsite/Resources/Public/CookieConsent/cookieconsent.min.js
         cookieconsentBase.title = CookieConsent 2 Basis
--- Resources/Public/Testsite/js/app.js
+++ Resources/Public/Testsite/js/app.js
@@ -1,18 +1,2 @@
 $(document).foundation();
 
-window.cookieconsent.initialise({
-    container: document.getElementById("content"),
-    palette:{
-        popup: {background: "#fff"},
-        button: {background: "#aa0000"},
-    },
-    revokable:true,
-    onStatusChange: function(status) {
-        console.log(this.hasConsented() ?
-            'enable cookies' : 'disable cookies');
-    },
-    law: {
-        regionalLaw: false,
-    },
-    location: true,
-});
